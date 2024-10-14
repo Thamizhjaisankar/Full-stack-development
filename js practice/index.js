9e592d231aaa8e0f04a221c917cd62e242c29999
Thamizhjaisankar <144304770+Thamizhjaisankar@users.noreply.github.com>
2024-10-14 12:10:05 +0530

    Add files via upload

---
+++ js practice/index.js
@@ -0,0 +1,8 @@
+alert("hello!! welcome to our website");
+let name=["thamizh",20];
+console.log(name);
+console.log(10+20);
+let myname={fname:"thamizh",
+         age:20,
+        gender:"female"};
+console.log(myname.fname);
